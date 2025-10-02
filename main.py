07a21904c77cd392a1f0a24e04255708c1a3da1b
jordanaconnor <jordanaconnor@gmail.com>
2025-10-02 11:27:21 -0600

    Finsihed player movement and rotation

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from player import Player
 def main():
     isRunnng = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updateable_group, drawable_group)
+    print(type(Player.containers))
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
@@ -12,14 +18,22 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    
+
+
     while isRunnng:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
         screen.fill(color="black")
-        player.draw(screen)
+        
+        updateable_group.update(dt)
+        
+        for object in drawable_group:
+            object.draw(screen)
+        
         print("Starting Asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")
