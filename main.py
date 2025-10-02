ac9671b9da85a239fb62a7e756bacdcd2e4922b6
jordanaconnor <jordanaconnor@gmail.com>
2025-10-02 09:45:57 -0600

    Add player and hitbox to game and screen

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
-import constants
+from constants import *
+from player import Player
 
 def main():
     isRunnng = True
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while isRunnng:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(color="black")
-
+        player.draw(screen)
         print("Starting Asteroids!")
-        print(f"Screen width: {constants.SCREEN_WIDTH}")
-        print(f"Screen height: {constants.SCREEN_HEIGHT}")
+        print(f"Screen width: {SCREEN_WIDTH}")
+        print(f"Screen height: {SCREEN_HEIGHT}")
+
 
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
